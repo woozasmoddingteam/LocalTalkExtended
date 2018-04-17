3a3f1a499693c3975d1c505df8c70d6d4a0d9f2e
Las <las@protonmail.ch>
2018-04-17 19:00:04 +0200

    use laspad.lua instead

---
+++ laspad.lua
@@ -0,0 +1,14 @@
+return {
+	master = {
+		name            = "Local Talk Extended",
+		tags            = {"must be run on server", "gameplay tweak"},
+		description     = laspad.get_description("README.md"),
+		preview         = laspad.read("preview.jpg"),
+	},
+	beta = {
+		name            = "Local Talk Extended - Beta",
+		tags            = {"must be run on server", "gameplay tweak", "beta"},
+		description     = "this is a beta - do not use",
+		preview         = laspad.read("preview.jpg"),
+	}
+}

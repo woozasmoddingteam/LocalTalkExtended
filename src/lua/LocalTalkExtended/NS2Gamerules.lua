380ce3adec752780198b9b74197b931bd9c25c23
Las <las@protonmail.ch>
2018-04-18 16:15:07 +0200

    Make work with latest shine
    Basically shine completely ignores the GetCanPlayerHearPlayer method in
    Gamerules, so we now have to replace the event hook itself that checks
    if a player can hear another player. Jesus, shine.

--- src/lua/LocalTalkExtended/NS2Gamerules.lua
+++ src/lua/LocalTalkExtended/NS2Gamerules.lua
@@ -4,19 +4,27 @@ if Server then
 	})
 
 	Server.HookNetworkMessage("LocalTalkExtended_teamonly", function(client, msg)
-		Log("%s: %s", client, msg.on)
 		voice_teamonly[client] = msg.on
 	end)
 
-	local kMaxWorldSoundDistance
+	local kMaxWorldSoundDistance = debug.getupvaluex(
+		NS2Gamerules.GetCanPlayerHearPlayer, "kMaxWorldSoundDistance")
 
-	local old = NS2Gamerules.GetCanPlayerHearPlayer
-	debug.replacemethod("NS2Gamerules", "GetCanPlayerHearPlayer",
-	function(self, listener, speaker, channel)
-		return old(self, listener, speaker, channel) or
-			channel ~= VoiceChannel.Global and
-			(Log("team_only: %s", voice_teamonly[speaker:GetClient()]) or true) and
-			not voice_teamonly[speaker:GetClient()] and
-			listener:GetDistanceSquaredToEntity(speaker) < kMaxWorldSoundDistance^2
-	end)
+	-- Fuck you shine
+	-- You're the reason I have to do this
+	-- Why can't you just be a normal fucking
+	-- mod?
+	local old = Event.Hook
+	function Event.Hook(hook, func, ...)
+		if hook == "CanPlayerHearPlayer" then
+			return old(hook, function(listener, speaker, channel)
+				return GetGamerules():GetCanPlayerHearPlayer(listener, speaker, channelType) or
+					channel ~= VoiceChannel.Global and
+					voice_teamonly[speaker:GetClient()] == false and
+					listener:GetDistanceSquaredToEntity(speaker) < kMaxWorldSoundDistance^2
+			end, ...)
+		else
+			return old(hook, func, ...)
+		end
+	end
 end

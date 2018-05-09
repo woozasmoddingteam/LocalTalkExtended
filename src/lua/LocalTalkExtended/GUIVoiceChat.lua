c9687dab79753c26353deaba8518bb526ad114e6
Las <las@protonmail.ch>
2018-05-09 17:15:30 +0200

    Remove speaker icon and fix some bugs
    Now the speaker icon that shows up next to the
    speakers' name is removed, since it was unnecessary
    and is going to allow more space to be used for other
    purposes.

--- src/lua/LocalTalkExtended/GUIVoiceChat.lua
+++ src/lua/LocalTalkExtended/GUIVoiceChat.lua
@@ -1,5 +1,13 @@
+local ClearAllBars
+local GetFreeBar
+local kBackgroundOffset
+local kBackgroundYSpace
+local kBackgroundSize
+local kBackgroundTexture
+local kVoiceChatIconOffset
+
 debug.replacemethod("GUIVoiceChat", "SendKeyEvent",
-function(key, down, amount)
+function(self, key, down, amount)
 	local player = Client.GetLocalPlayer()
 
 	if down then
@@ -30,12 +38,8 @@ function(key, down, amount)
 	end
 end)
 
-local kGlobalSpeakerIcon
-local ClearAllBars
-local GetFreeBar
-local kBackgroundOffset
 debug.replacemethod("GUIVoiceChat", "Update",
-function(delta_time)
+function(self, delta_time)
 	PROFILE("GUIVoiceChat:Update")
 
 	if self.recordEndTime and self.recordEndTime < Shared.GetTime() then
@@ -49,25 +53,24 @@ function(delta_time)
 	local bar_position = Vector(kBackgroundOffset)
 
 	for i = 1, #players do
+		local player = players[i]
+
+		local client_index = player.ClientIndex
+		local channel = client_index and ChatUI_GetVoiceChannelForClient(client_index) or VoiceChannel.Invalid
+
 		if channel ~= VoiceChannel.Invalid then
 			local bar = GetFreeBar(self)
 
-			local team = players[i].EntityTeamNumber
-
-			local client_index = players[i].ClientIndex
-			local channel      = client_index and ChatUI_GetVoiceChannelForClient(client_index) or VoiceChannel.Invalid
+			local team = player.EntityTeamNumber
 
 			local color =
-				players[i].IsCommander and GUIVoiceChat.kCommanderFontColor or
+				player.IsCommander and GUIVoiceChat.kCommanderFontColor or
 				team == 1 and GUIVoiceChat.kMarineFontColor or
 				team == 2 and GUIVoiceChat.kAlienFontColor or
 				GUIVoiceChat.kSpectatorFontColor
 
-			bar.Name:SetText(players[i].Name)
+			bar.Name:SetText(player.Name)
 			bar.Name:SetColor(color)
-
-			bar.Icon:SetTexture(kGlobalSpeakerIcon)
-			bar.Icon:SetColor(color)
 
 			bar.Background:SetTexture(string.format(kBackgroundTexture, team == 2 and "alien" or "marine"))
 			bar.Background:SetColor(team ~= 1 and team ~= 2 and Color(1, 200/255, 150/255, 1) or Color(1, 1, 1, 1))
@@ -78,4 +81,34 @@ function(delta_time)
 			bar_position.y = bar_position.y + kBackgroundSize.y + kBackgroundYSpace
 		end
 	end
+end)
+
+debug.replaceupvalue(debug.getupvaluex(GUIVoiceChat.Update, "GetFreeBar"), "CreateChatBar",
+function()
+	local background = GUIManager:CreateGraphicItem()
+	background:SetSize(kBackgroundSize)
+	background:SetAnchor(GUIItem.Right, GUIItem.Center)
+	background:SetPosition(kBackgroundOffset)
+	background:SetIsVisible(false)
+
+	local name = GUIManager:CreateTextItem()
+	name:SetFontName(Fonts.kAgencyFB_Small)
+	name:SetAnchor(GUIItem.Right, GUIItem.Center)
+	name:SetScale(GetScaledVector())
+	name:SetTextAlignmentX(GUIItem.Align_Max)
+	name:SetTextAlignmentY(GUIItem.Align_Center)
+	name:SetPosition(Vector(-GUIScale(30), 0, 0))
+	GUIMakeFontScale(name)
+	background:AddChild(name)
+
+	return {Background = background, Name = name}
+end)
+
+debug.replaceupvalue(GUIVoiceChat.Uninitialize, "DestroyChatBar",
+function(bar)
+	GUI.DestroyItem(bar.Name)
+	bar.Name = nil
+
+	GUI.DestroyItem(bar.Background)
+	bar.Background = nil
 end)

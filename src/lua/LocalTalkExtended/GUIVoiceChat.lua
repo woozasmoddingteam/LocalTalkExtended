63fab7ed04eebc68c0cbe225d615d9937df91718
Las <las@protonmail.ch>
2018-05-12 12:55:57 +0200

    Fix a bunch of bugs with voice chat bars
    Now the voice chat bar will appear a little late
    if the voice channel is proximity, since extra information
    is needed in that case.
    
    Whenever a channel change happens, the voice chat bar will also
    reset now, which it didn't do before unless there was a brief window
    where the person wasn't talking.

--- src/lua/LocalTalkExtended/GUIVoiceChat.lua
+++ src/lua/LocalTalkExtended/GUIVoiceChat.lua
@@ -86,9 +86,27 @@ function GUIVoiceChat:OnResolutionChanged()
 	self:Initialize()
 end
 
+local function ResetBarForPlayerRecord(player)
+	local bar = player.VoiceChatBar
+	if not bar then return end
+
+	player.VoiceChatBar = nil
+	player.VoiceChannel = nil
+	bar.player = nil
+	bar.background:SetIsVisible(false)
+
+	Log("Reset bar for player %s", player.ClientIndex)
+end
+
+local function ResetBarForClient(client)
+	local player = Scoreboard_GetPlayerRecord(client)
+	return ResetBarForPlayerRecord(player)
+end
+
 local team_only
 function GUIVoiceChat:SendKeyEvent(key, down, amount)
 	local player = Client.GetLocalPlayer()
+	local client = Client.GetLocalClientIndex()
 
 	if down then
 		if ChatUI_EnteringChatMessage() then return end
@@ -97,11 +115,13 @@ function GUIVoiceChat:SendKeyEvent(key, down, amount)
 		if GetIsBinding(key, bind) then
 			self.recordBind    = bind
 			self.recordEndTime = nil
+			ResetBarForClient(client)
 
 			Client.VoiceRecordStartGlobal()
 		elseif GetIsBinding(key, "LocalVoiceChat") then
 			self.recordBind    = "LocalVoiceChat"
 			self.recordEndTime = nil
+			ResetBarForClient(client)
 
 			if team_only ~= false then
 				team_only = false
@@ -111,6 +131,7 @@ function GUIVoiceChat:SendKeyEvent(key, down, amount)
 		elseif GetIsBinding(key, "LocalVoiceChatTeam") then
 			self.recordBind    = "LocalVoiceChatTeam"
 			self.recordEndTime = nil
+			ResetBarForClient(client)
 
 			if team_only ~= true then
 				team_only = true
@@ -128,12 +149,15 @@ local voice_teamonly = table.array(100)
 
 Client.HookNetworkMessage("LocalTalkExtended_teamonly_notify", function(msg)
 	voice_teamonly[msg.client] = msg.on
+	ResetBarForClient(msg.client)
 end)
 
 function GUIVoiceChat:Update(delta_time)
 	PROFILE("GUIVoiceChat:Update")
 
-	if self.recordEndTime and self.recordEndTime < Shared.GetTime() then
+	local time = Shared.GetTime()
+
+	if self.recordEndTime and self.recordEndTime < time then
 		Client.VoiceRecordStop()
 		self.recordEndTime = nil
 	end
@@ -149,10 +173,14 @@ function GUIVoiceChat:Update(delta_time)
 			-- Can we call this for invalid clients?
 			local client  = player.ClientIndex
 			local channel = ChatUI_GetVoiceChannelForClient(client) or VoiceChannel.Invalid
-			if channel == VoiceChannel.Invalid then
-				player.VoiceChatBar = nil
-				bar.player = nil
-				bar.background:SetIsVisible(false)
+			if channel ~= player.VoiceChannel then
+				-- If the channel isn't the global one, i.e. if it's proximity,
+				-- we also need the network message to tell us what kind of
+				-- local voice chat it is, so we delay the bar here.
+				if channel ~= VoiceChannel.Global then
+					player.VoiceChatBarTime = nil
+				end
+				ResetBarForPlayerRecord(player)
 			end
 		end
 	end
@@ -163,39 +191,58 @@ function GUIVoiceChat:Update(delta_time)
 		local client  = player.ClientIndex
 		local channel = client and ChatUI_GetVoiceChannelForClient(client) or VoiceChannel.Invalid
 
+		-- Edge case bug:
+		-- If there are not any bars left, a player not speaking
+		-- will not have their VoiceChatBarTime set to nil
 		if channel ~= VoiceChannel.Invalid and not player.VoiceChatBar then
-			local bar
-			for i = 1, #chat_bars do
-				if not chat_bars[i].client then
-					bar = chat_bars[i]
+			local create_bar = true
+			if channel ~= VoiceChannel.Global and client ~= local_client then
+				-- We need to do this because the team-only network message
+				-- arrives after the voice transmission begins
+				local bar_time = player.VoiceChatBarTime
+				if not bar_time then
+					player.VoiceChatBarTime = time + 0.15
+					create_bar = false
+				else
+					create_bar = bar_time <= time
 				end
 			end
-			-- All bars may be occupied
-			if bar then
-				local team = player.EntityTeamNumber
+			if create_bar then
+				local bar
+				for i = 1, #chat_bars do
+					if not chat_bars[i].client then
+						bar = chat_bars[i]
+					end
+				end
+				-- All bars may be occupied
+				if bar then
+					local team = player.EntityTeamNumber
 
-				local color =
-					channel ~= VoiceChannel.Global and (
-						team == local_team and (
-							client == local_client and team_only or voice_teamonly[client]
-						) and kLocalVoiceTeamOnlyFontColor or kLocalVoiceFontColor
-					) or
-					player.IsCommander and GUIVoiceChat.kCommanderFontColor or
-					team == 1 and GUIVoiceChat.kMarineFontColor or
-					team == 2 and GUIVoiceChat.kAlienFontColor or
-					GUIVoiceChat.kSpectatorFontColor
+					local color =
+						channel ~= VoiceChannel.Global and (
+							team == local_team and (
+								client == local_client and team_only or voice_teamonly[client]
+							) and kLocalVoiceTeamOnlyFontColor or kLocalVoiceFontColor
+						) or
+						player.IsCommander and GUIVoiceChat.kCommanderFontColor or
+						team == 1 and GUIVoiceChat.kMarineFontColor or
+						team == 2 and GUIVoiceChat.kAlienFontColor or
+						GUIVoiceChat.kSpectatorFontColor
 
-				bar.name:SetText(player.Name)
-				bar.name:SetColor(color)
+					bar.name:SetText(player.Name)
+					bar.name:SetColor(color)
 
-				bar.icon:SetColor(color)
+					bar.icon:SetColor(color)
 
-				bar.background:SetTexture(team == 2 and kBackgroundTextureAlien or kBackgroundTextureMarine)
-				bar.background:SetColor(team ~= 1 and team ~= 2 and Color(1, 200/255, 150/255, 1) or Color(1, 1, 1, 1))
-				bar.background:SetIsVisible(self.visible)
+					bar.background:SetTexture(team == 2 and kBackgroundTextureAlien or kBackgroundTextureMarine)
+					bar.background:SetColor(team ~= 1 and team ~= 2 and Color(1, 200/255, 150/255, 1) or Color(1, 1, 1, 1))
+					bar.background:SetIsVisible(self.visible)
 
-				player.VoiceChatBar = true
-				bar.player = player
+					player.VoiceChatBar = bar
+					bar.player = player
+
+					player.VoiceChannel = channel
+				end
 			end
 		end
 	end

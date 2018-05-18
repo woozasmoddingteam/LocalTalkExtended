3c9430e045e0a10672f3ca1a573322112236dc86
Las <las@protonmail.ch>
2018-05-18 20:10:31 +0200

    Move basecommands override to separate file and fix file loading bug
    It did not work when loading the same file multiple times

--- src/lua/LocalTalkExtended/FileHooks.lua
+++ src/lua/LocalTalkExtended/FileHooks.lua
@@ -8,4 +8,5 @@ for _, v in ipairs {
 end
 
 ModLoader.SetupFileHook("lua/GUIVoiceChat.lua", "lua/LocalTalkExtended/GUIVoiceChat.lua", "replace")
+ModLoader.SetupFileHook("lua/shine/extensions/basecommands/server.lua", "lua/LocalTalkExtended/basecommands.lua", "post")
 ModLoader.SetupFileHook("lua/NS2Plus/Client/CHUD_Options.lua", "lua/LocalTalkExtended/CHUD_Options.lua", "post")

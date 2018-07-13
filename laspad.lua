69f32a6fe951b33dbe98072cab8d85a5074354ca
Las <las@protonmail.ch>
2018-07-13 16:33:51 +0200

    Add website to laspad.lua

--- laspad.lua
+++ laspad.lua
@@ -4,6 +4,7 @@ return {
 		tags            = {"must be run on server", "gameplay tweak"},
 		description     = laspad.get_description("README.md", true, "https://github.com/woozasmoddingteam/LocalTalkExtended"),
 		preview         = laspad.read("preview.jpg"),
+		website         = "https://github.com/woozasmoddingteam/LocalTalkExtended",
 	},
 	beta = {
 		name            = "Local Talk Extended - Beta",

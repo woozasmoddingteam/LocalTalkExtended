6a79671a04ccaa6086e3c92ea56754947dca2c71
Las <las@protonmail.ch>
2019-05-19 19:17:03 +0200

    Move bindings to the top of the options menu
    This also works around a limit in vanilla NS2 wrt.
    the amount of bindings in the options menu.

--- src/lua/LocalTalkExtended/BindingsDialog.lua
+++ src/lua/LocalTalkExtended/BindingsDialog.lua
@@ -2,10 +2,10 @@ local defaults = debug.getupvaluex(GetDefaultInputValue, "defaults")
 table.insert(defaults, {"LocalVoiceChat", "None"})
 table.insert(defaults, {"LocalVoiceChatTeam", "None"})
 
-local bindings = debug.getupvaluex(BindingsUI_GetBindingsData, "globalControlBindings")
-for _, v in ipairs {
+local bindings = BindingsUI_GetBindingsData()
+for i, v in ipairs {
 	"LocalVoiceChat",     "input", "Proximity Communication (can be heard by enemy)",     "None",
 	"LocalVoiceChatTeam", "input", "Proximity Communication (can be heard by team only)", "None",
 } do
-	table.insert(bindings, v)
+	table.insert(bindings, i, v)
 end
